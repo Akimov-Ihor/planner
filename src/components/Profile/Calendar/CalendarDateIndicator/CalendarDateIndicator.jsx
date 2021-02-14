290b0c726d257afd0f4c94b44bf38a20c6bd07c5
Akimov-Ihor <pandable1996@gmail.com>
2021-02-14 02:18:42 +0200

    [Add]calendar's basic styles

--- src/components/Profile/Calendar/CalendarDateIndicator/CalendarDateIndicator.jsx
+++ src/components/Profile/Calendar/CalendarDateIndicator/CalendarDateIndicator.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {
   getDayOfMonth,
-  getMonthDayYear,
+  getToday,
   getMonth,
   getYear,
 } from '../../../utils/date-moment';
+import moment from 'moment'
 import { getDatesInMonthDisplay } from '../../../utils/date-utils';
 import './CalendarDateIndicator.css'
 
@@ -20,20 +21,21 @@ const CalendarDateIndicator = ({ activeDates, selectDate, setSelectDate }) => {
     getMonth(selectDate) + 1,
     getYear(selectDate)
   );
-
+ console.log(getToday() - selectDate , selectDate)
 
   return (<div className="calendar-date">
     {
       datesInMonth.map((i, key) => {
         return (
           <div
-            className="calendar-date-indicator"
+            className={
+              `calendar-date-indicator  ${i.currentMonth ? 'active' : 'non-active'} ${getToday() == selectDate ? 'today' : 'others'} `}
             data-active-month={i.currentMonth}
             data-date={i.date.toString()}
             key={key}
             onClick={changeDate}
           >
-           
+          
               {getDayOfMonth(i.date)}
             
 
